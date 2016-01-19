326d395a71519e127043438677fdc4922c3b7a58
Hannan Gigih Prastawa <hannanxp@gmail.com>
2016-01-19 17:12:31 +0700

    - CRUD controller

---
+++ tutorial03/crud.js
@@ -0,0 +1,59 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('CrudController', ['$scope', function ($scope) {
+    
+    var uid = 1;
+    
+    $scope.contacts = [
+      {id: 0, 'name': 'Viral',
+        'email': 'hello@gmail.com',
+        'phone': '123-2343-44'
+      }
+    ];
+
+    $scope.saveContact = function () {
+
+      if ($scope.newcontact.id == null) {
+        //if this is new contact, add it in contacts array
+        $scope.newcontact.id = uid++;
+        $scope.contacts.push($scope.newcontact);
+      } else {
+        //for existing contact, find this contact using id
+        //and update it.
+        for (i in $scope.contacts) {
+          if ($scope.contacts[i].id == $scope.newcontact.id) {
+            $scope.contacts[i] = $scope.newcontact;
+          }
+        }
+      }
+
+      //clear the add contact form
+      $scope.newcontact = {};
+    };
+
+
+    $scope.delete = function (id) {
+
+      //search contact with given id and delete it
+      for (i in $scope.contacts) {
+        if ($scope.contacts[i].id == id) {
+          $scope.contacts.splice(i, 1);
+          $scope.newcontact = {};
+        }
+      }
+
+    };
+
+
+    $scope.edit = function (id) {
+      //search contact with given id and update it
+      for (i in $scope.contacts) {
+        if ($scope.contacts[i].id == id) {
+          //we use angular.copy() method to create 
+          //copy of original object
+          $scope.newcontact = angular.copy($scope.contacts[i]);
+        }
+      }
+    };
+  }]);
+
